98b74ce1ac3b2e1cdd4cfa3e9907f543775f67aa
smei16 <smei16@binghamton.edu>
2023-04-24 18:13:50 -0400

    checkout changes

--- Toy Project/js/checkout.js
+++ Toy Project/js/checkout.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     if (localStorage.length !== 0) {
         document.querySelector('#emptycartsection').style.display='none';
     }
-    if (localStorage.length == 0) {
-        document.querySelector('#cartsection').style.display='none';
+    if (localStorage.length === 0) {
+        document.querySelector('#cartSection').style.display='none';
     }   else{
         const resetBtn = document.querySelector('#clearCartBtn');
         resetBtn.addEventListener('click', function(){
